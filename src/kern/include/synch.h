c735a9837a0c09daa2b98096a314ab86a4f79df9
Chris Short <shortwchris@gmail.com>
2019-03-25 21:47:24 -0400

    Add variables to lock struct

--- src/kern/include/synch.h
+++ src/kern/include/synch.h
@@ -75,7 +75,13 @@ void V(struct semaphore *);
 struct lock {
         char *lk_name;
         HANGMAN_LOCKABLE(lk_hangman);   /* Deadlock detector hook. */
+        
         // add what you need here
+        struct wchan *lk_wchan;
+        struct spinlock lk_lock;
+        volatile enum { zero, one } value;
+        struct thread lk_holder;
+
         // (don't forget to mark things volatile as needed)
 };
 
