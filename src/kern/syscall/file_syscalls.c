8c079e7c09df6714403d57a4f3246a199d4a0cf9
David W  McCreight <dmccreig@felix.cs.wm.edu>
2019-04-26 19:11:08 -0400

    Add potentially unecessary  stubs for file syscalls

--- src/kern/syscall/file_syscalls.c
+++ src/kern/syscall/file_syscalls.c
@@ -12,3 +12,39 @@ int chdir() {
 int getcwd() {
     return 0;
 }
+
+
+
+///////////////////////////
+//  Unsure if necessary  //
+//                       //
+
+int open() {
+    return 0;
+}
+
+int read() {
+    return 0;
+}
+
+int write() {
+    return 0;
+}
+
+int lseek() {
+    return 0;
+}
+
+int close() {
+    return 0;
+}
+
+int dup2() {
+    return 0;
+}
+   
+//                       //
+//  Unsure if necessary  //
+///////////////////////////
+
+
