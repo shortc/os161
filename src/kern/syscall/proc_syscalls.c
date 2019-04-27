0a158d02661f0d249ef0f35f81b9f90992dcee28
David W  McCreight <dmccreig@felix.cs.wm.edu>
2019-04-27 19:39:01 -0400

    Add stubs and some functionality for proc syscalls

--- src/kern/syscall/proc_syscalls.c
+++ src/kern/syscall/proc_syscalls.c
@@ -3,11 +3,35 @@
  */
 
 
-#include <unistd.h>
+//#include <unistd.h>
 #include <current.h>
 #include <proc.h>
 
-int sys_getpid(pid_t curpid) {
-    curpid = curproc->pid;
+int sys_getpid(int32_t *retval) {
+    *retval = (int32_t)curproc->pid;
     return 0;
+}
+
+
+int sys_fork(/* other stuff? */int *retval) {
+    
+	sys_getpid(&id);
+    
+	return 0;
+}
+
+int sys_execv(const char *path, char *const argv[]) {
+    
+	return 0;
+}
+
+int sys_waitpid(pid_t pid, int *status, int options, int *retval) {
+    
+	return 0;
+}
+
+int sys___exit(int retcode = 0) {
+    
+	thread_exit();
+	return 0;
 }
